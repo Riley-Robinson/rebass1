c23180a07de88d37a50ae1b5c9f909aff6a54b8b
Riley-Robinson <rrtoinfinity@gmail.com>
2021-01-06 15:13:41 -0500

    basic card showing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  Box,
+  Card,
+  Image,
+  Heading,
+  Text
+} from 'rebass'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default ({
+  image,
+  title,
+  description
+}) =>
+  <Box width={256}>
+    <Card
+      sx={{
+        p:1,
+        borderRadius: 2,
+        boxShadow: '0 0 16px rgba(0, 0, 0, .25)',
+      }}>
+        <Image src={image} />
+        <Box px={2}>
+            <Heading as='h3'>
+              {title}
+            </Heading>
+            <Text fontSize={0}>
+              {description}
+            </Text>
+        </Box>
+      </Card>
+  </Box>
